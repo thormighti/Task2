5c590e64f62c059e7c05e2a0a01602d20db5b641
Peter <peterabeshi4@gmail.com>
2022-03-10 14:35:48 +0100

    updated files

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,8 +12,8 @@ module.exports = {
       url:"127.0.0.1:8545"
     },
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/kBQgiKvhhtPp2_6TrHyrdRpeq2USykU3",
-      accounts: ["a529f08f5b76492a9198ef59bdf28304d56316999c4ecc96ffdc697e7fb0a82a"],
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [RINKEBY_PRIVATE_KEY],
     },
   },
 };
